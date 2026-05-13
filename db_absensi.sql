7ffd1a54539b1b513b59893282d18ea37c736640
M Dzackwan Dzakky <dzakky9102@gmail.com>
2026-05-13 07:12:47 +0700

    Update db_absensi.sql

--- db_absensi.sql
+++ db_absensi.sql
@@ -1,10 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
 -- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 12 Bulan Mei 2026 pada 22.07
--- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -17,16 +11,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `db_absensi`
---
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `absensi`
---
-
 CREATE TABLE `absensi` (
   `id_absen` int(11) NOT NULL,
   `username` varchar(50) DEFAULT NULL,
@@ -36,20 +20,12 @@ CREATE TABLE `absensi` (
   `keterangan` enum('Hadir','Izin','Sakit') DEFAULT 'Hadir'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data untuk tabel `absensi`
---
 
 INSERT INTO `absensi` (`id_absen`, `username`, `tanggal`, `jenis_absen`, `jam_masuk`, `keterangan`) VALUES
 (1, 'Anas', '2026-05-13', 'Pagi', '09:15:00', 'Hadir'),
 (2, 'Nisa', '2026-05-13', 'Pagi', '09:20:00', 'Hadir'),
 (3, 'Anas', '2026-05-13', 'Siang', '12:05:00', 'Hadir');
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `users`
---
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -60,9 +36,6 @@ CREATE TABLE `users` (
   `divisi` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data untuk tabel `users`
---
 
 INSERT INTO `users` (`id`, `username`, `password`, `role`, `nama_lengkap`, `divisi`) VALUES
 (1, 'jaloeeyyy', 'jaloe0085', 'Admin', 'Administrator Utama', 'Dev'),
@@ -70,47 +43,20 @@ INSERT INTO `users` (`id`, `username`, `password`, `role`, `nama_lengkap`, `divi
 (3, 'Nisa', 'user123', 'HR', 'Nisa Lubis', 'HRD'),
 (4, 'Anas', 'cs123', 'Karyawan', 'Anas', 'CS');
 
---
--- Indexes for dumped tables
---
-
---
--- Indeks untuk tabel `absensi`
---
 ALTER TABLE `absensi`
   ADD PRIMARY KEY (`id_absen`),
   ADD KEY `fk_user_absen` (`username`);
 
---
--- Indeks untuk tabel `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `absensi`
---
 ALTER TABLE `absensi`
   MODIFY `id_absen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- AUTO_INCREMENT untuk tabel `users`
---
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `absensi`
---
 ALTER TABLE `absensi`
   ADD CONSTRAINT `fk_user_absen` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
